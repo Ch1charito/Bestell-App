23078df96172abdd0a08505fb774ec7421b69445
Ch1charito <r.buschmann.work@gmail.com>
2025-04-15 16:37:34 +0200

    added Hauptgerichte

---
+++ script.js
@@ -0,0 +1,24 @@
+let hauptgerichte = [
+    {
+        "name": "Spaghetti alle Vongole",
+        "price": 17.99,
+        "descripton": "Spaghetti mit frischen Venusmuscheln in einer leckeren Weißweinsoße"
+    },
+    {
+        "name": "Pizza Margherita",
+        "price": 12.99,
+        "descripton": "eine knusprige Steinofenpizza mit Büffel-Mozarella"
+    },
+    
+    {
+        "name": "Spaghetti Carbonara",
+        "price": 14.25,
+        "descripton": "Spaghetti mit frischem Guanciale und Pecorino Romano"
+    },
+    {
+        "name": "Risotto ai funghi",
+        "price": 16.75,
+        "descripton": "Risotto in einer Weißweinsoße mit Steinpilzen und frischem Parmesan"
+    },
+]
+
