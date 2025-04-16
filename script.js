f90b8362344e1062f2f39dc7ad752402841c3c51
Ch1charito <r.buschmann.work@gmail.com>
2025-04-16 18:15:19 +0200

    added an addtocart function and render cart function

--- script.js
+++ script.js
@@ -20,8 +20,10 @@ let hauptgerichte = [
         "price": 16.75,
         "descripton": "Risotto in einer Weißweinsoße mit Steinpilzen und frischem Parmesan"
     },
-]
+];
 
+
+let cart = [];
 // ich möchte eine render Function die mir alle daten zu meinen Hauptgerichten rendert
 
 function renderHauptgerichte() {
@@ -37,7 +39,7 @@ function renderHauptgerichte() {
 
 function getGerichteTemplate(index) {
     return `<div class="abstand border">
-                <div class="name-button kleiner-abstand einrücken"><h4 id="name-${index}">name</h4><button class="add-button">+</button></div>
+                <div class="name-button kleiner-abstand einrücken"><h4 id="name-${index}">name</h4><button onclick="addToCart(${index})" class="add-button">+</button></div>
                 <p id="description-${index}" class="einrücken kleiner-abstand">beschreibung</p>
                 <p id="price-${index}" class="einrücken kleiner-abstand">preis</p>
             </div>`
@@ -61,4 +63,70 @@ function renderPrice(index) {
     let priceRef = document.getElementById(`price-${index}`);
     priceRef.innerHTML = hauptgerichte[index].price + " €";
     
+}
+
+// ich brauche eine function die ich per onclick aufrufe immer wenn ich auf den button drücke zum hinzufügen des item --> dabei soll der name und der preis übergeben werden und in einem neuen template angezeigt werden
+
+/* function addToCart(index) {
+    let nameInput = document.getElementById(`name-${index}`).innerText;
+    let priceInput = document.getElementById(`price-${index}`).innerText;
+    cart.push({
+        name: nameInput,
+        price: priceInput,
+        amount: 1,
+    });
+    console.log(cart);
+    
+    
+}
+console.log(cart); */
+
+// ich brauche eine function um herauszufinden ob der name sich schon in cart befindet
+
+
+function getNameIndex(name) {                               // eine function um zu prüfen ob der name bereits im array vergeben ist und wenn dann auch wo
+    function checkItem(item) {                              // eine function innerhalb der function um jedes objekt im array zu prüfen
+        return item.name === name;                          // vergleicht ob der name in dem array mit objekten bereits verwendet wird --> gibt ture oder flase aus
+    }
+
+    let nameIndex = cart.findIndex(checkItem);              // gibt nameIndex den Index im array cart wenn true bei checkItem rauskommt und -1 wenn false
+    return nameIndex;
+}
+
+//ich muss die functionen verbinden und mache deswegen eine neue die erste lasse ich oben zur übersicht trotdem erstmal stehen
+
+function addToCart(index) {
+    let nameInput = document.getElementById(`name-${index}`).innerText;         // nameInput hat den aktuellen wert, da wir ihn in der template über die dynamische id mitgeben
+    let priceInput = document.getElementById(`price-${index}`).innerText;       // das selbe nur bei price
+    let nameIndex = getNameIndex(nameInput);                                    // wir geben nameIndex den wert aus der function getNameIndex mit dem parameter aus nameInput und kriegen als ergebnis entweder den index oder -1 raus
+    if (nameIndex === -1) {                                                     // wenn es also noch nicht im array ist wird dem cart array mehrere objekt hinzugefügt
+        cart.push({
+        name: nameInput,
+        price: priceInput,
+        amount: 1,
+    });
+    } else {
+        cart[nameIndex].amount++;                                               // sonst wenn es forhanden ist wird nur amount +1 erhöht
+    }
+    
+    console.log(cart);
+    renderCart();
+}
+
+// ich brauche eine function um mein cart array auch zu rendern
+
+function renderCart() {
+    let cartRef = document.getElementById('cart-content')
+    for (let i = 0; i < cart.length; i++) {
+        cartRef.innerHTML += getCartTemplate(i);
+    }
+    
+}
+
+// ein template für das was in dem warenkorb drinnen ist
+function getCartTemplate(index){
+    return `<div>
+                <p>gericht name</p>
+                <div><button>-</button><p>amount</p><button>+</button><p>price</p><button>clear</button></div>
+            </div>`
 }
